7439f53fe4bed541fbe3f886d268ce804e7c3aa2
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-05-17 16:38:13 +0300

    fix(Epic): исправить code style

--- src/ru/yandex/practicum/tasks/Epic.java
+++ src/ru/yandex/practicum/tasks/Epic.java
@@ -13,6 +13,7 @@ public class Epic extends Task {
         super(id, name, description, status);
         subtaskIDs = new ArrayList<>();
     }
+
     // Конструктор класса Epic
     public Epic(int id, String name, String description, TaskStatus status, List<Integer> subtaskIDs) {
         this(id, name, description, status);
@@ -34,7 +35,7 @@ public class Epic extends Task {
         String resultSuper = super.toString();
         String contentSuper = resultSuper.substring(resultSuper.indexOf("{"), resultSuper.indexOf("}"));
         String result = "Epic" + contentSuper;
-        result += subtaskIDs.isEmpty() ? ", subtasks=empty}" : (", subtasks.size=" + subtaskIDs.size() + "}") ;
+        result += subtaskIDs.isEmpty() ? ", subtasks=empty}" : (", subtasks.size=" + subtaskIDs.size() + "}");
 
         return result;
     }
