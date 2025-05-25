55bcfd71b4ef341731810a449c3404aebc122f27
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-05-25 11:59:10 +0300

    feat(FileBackedTaskManager): создать метод сохранения задач, переопределить все модифицирующие операции, добавить в них указанный метод

--- src/ru/yandex/practicum/taskManagement/FileBackedTaskManager.java
+++ src/ru/yandex/practicum/taskManagement/FileBackedTaskManager.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.taskManagement;
 
+import ru.yandex.practicum.tasks.*;
+
 public class FileBackedTaskManager extends InMemoryTaskManager {
 
     // Путь к файлу автосохранения
@@ -15,5 +17,91 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
     public FileBackedTaskManager(HistoryManager historyManager, String fileName) {
         super(historyManager);
         this.fileName = fileName;
+    }
+
+    // Сохранение всех задач в файл
+    private void save() {
+
+    }
+
+    // Добавление новой задачи (обычной)
+    @Override
+    public void addBasicTask(Task task) {
+        super.addBasicTask(task);
+        save();
+    }
+
+    // Добавление новой поздадачи
+    @Override
+    public void addSubtask(Subtask subtask) {
+        super.addSubtask(subtask);
+        save();
+    }
+
+    // Добавление нового эпика
+    @Override
+    public void addEpic(Epic epic) {
+        super.addEpic(epic);
+        save();
+    }
+
+    // Обновление задачи (обычной)
+    @Override
+    public void updateBasicTask(Task updatedTask) {
+        super.updateBasicTask(updatedTask);
+        save();
+    }
+
+    // Обновление подзадачи
+    @Override
+    public void updateSubtask(Subtask updatedSubtask) {
+        super.updateSubtask(updatedSubtask);
+        save();
+    }
+
+    // Обновление эпика
+    @Override
+    public void updateEpic(Epic updatedEpic) {
+        super.updateEpic(updatedEpic);
+        save();
+    }
+
+    @Override
+    public void removeBasicTaskById(int id) {
+        super.removeBasicTaskById(id);
+        save();
+    }
+
+    @Override
+    public void removeSubtaskById(int id) {
+        super.removeSubtaskById(id);
+        save();
+    }
+
+    @Override
+    public void removeEpicById(int id) {
+        super.removeEpicById(id);
+        save();
+    }
+
+    // Удаление всех задач (обычных)
+    @Override
+    public void removeAllBasicTasks() {
+        super.removeAllBasicTasks();
+        save();
+    }
+
+    // Удаление всех подзадач
+    @Override
+    public void removeAllSubtasks() {
+        super.removeAllSubtasks();
+        save();
+    }
+
+    // Удаление всех эпиков
+    @Override
+    public void removeAllEpics() {
+        super.removeAllEpics();
+        save();
     }
 }
