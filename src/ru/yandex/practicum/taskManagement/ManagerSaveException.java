819df7bc686fdbbd7047eed19f9fd6c011e07efc
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-05-25 15:46:36 +0300

    refactor(ManagerSaveException): удалить неиспользуемый конструктор

--- src/ru/yandex/practicum/taskManagement/ManagerSaveException.java
+++ src/ru/yandex/practicum/taskManagement/ManagerSaveException.java
@@ -2,11 +2,6 @@ package ru.yandex.practicum.taskManagement;
 
 public class ManagerSaveException extends RuntimeException {
     // Конструктор класса ManagerSaveException
-    public ManagerSaveException() {
-
-    }
-
-    // Конструктор класса ManagerSaveException
     public ManagerSaveException(String message) {
         super(message);
     }
