f26681f1923beb36824a317deda9802ca1472fbc
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-05-25 15:46:14 +0300

    feat(ManagerLoadException): создать непроверяемое исключение ManagerLoadException

---
+++ src/ru/yandex/practicum/taskManagement/ManagerLoadException.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.taskManagement;
+
+public class ManagerLoadException extends RuntimeException {
+    // Конструктор класса ManagerLoadException
+    public ManagerLoadException(String message) {
+        super(message);
+    }
+}
