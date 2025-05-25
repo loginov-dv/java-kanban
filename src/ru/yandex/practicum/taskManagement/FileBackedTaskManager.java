2adeecc06518e80256bb752df733a9dfe18e9d69
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-05-25 11:49:07 +0300

    feat(FileBackedTaskManager): добавить конструкторы и поле для файла автосохранения

--- src/ru/yandex/practicum/taskManagement/FileBackedTaskManager.java
+++ src/ru/yandex/practicum/taskManagement/FileBackedTaskManager.java
@@ -1,4 +1,19 @@
 package ru.yandex.practicum.taskManagement;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
+
+    // Путь к файлу автосохранения
+    private final String fileName;
+
+    // Конструктор класса FileBackedTaskManager
+    public FileBackedTaskManager(String fileName) {
+        super();
+        this.fileName = fileName;
+    }
+
+    // Конструктор класса FileBackedTaskManager
+    public FileBackedTaskManager(HistoryManager historyManager, String fileName) {
+        super(historyManager);
+        this.fileName = fileName;
+    }
 }
