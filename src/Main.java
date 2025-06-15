ab0075baafe47b5b1d5ac72b78932040589e6feb
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-06-15 20:50:47 +0300

    feat(Main): заменить конструкторы задач

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import ru.yandex.practicum.taskManagement.*;
 import ru.yandex.practicum.tasks.*;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,22 +11,29 @@ public class Main {
 
         System.out.println("=> Создайте две задачи, эпик с тремя подзадачами и эпик без подзадач:\n");
 
-        Task task1 = new Task(inMemoryTaskManager.nextId(), "Задача 1", "Описание", TaskStatus.NEW);
-        Task task2 = new Task(inMemoryTaskManager.nextId(), "Задача 2", "Описание", TaskStatus.NEW);
+        Task task1 = new Task(inMemoryTaskManager.nextId(), "Задача 1", "Описание", TaskStatus.NEW,
+                LocalDateTime.now(), Duration.ofMinutes(60));
+        Task task2 = new Task(inMemoryTaskManager.nextId(), "Задача 2", "Описание", TaskStatus.NEW,
+                LocalDateTime.now(), Duration.ofMinutes(60));
         inMemoryTaskManager.addBasicTask(task1);
         inMemoryTaskManager.addBasicTask(task2);
 
-        Epic epic1 = new Epic(inMemoryTaskManager.nextId(), "Эпик 1", "Описание", TaskStatus.NEW);
-        Epic epic2 = new Epic(inMemoryTaskManager.nextId(), "Эпик 2", "Описание", TaskStatus.NEW);
+        Epic epic1 = new Epic(inMemoryTaskManager.nextId(), "Эпик 1", "Описание", TaskStatus.NEW,
+                null, Duration.ZERO);
+        Epic epic2 = new Epic(inMemoryTaskManager.nextId(), "Эпик 2", "Описание", TaskStatus.NEW,
+                null, Duration.ZERO);
         inMemoryTaskManager.addEpic(epic1);
         inMemoryTaskManager.addEpic(epic2);
 
         Subtask subtask11 = new Subtask(inMemoryTaskManager.nextId(), "Подзадача 11", "Описание",
-                TaskStatus.NEW, epic1.getID());
+                TaskStatus.NEW, epic1.getID(),
+                LocalDateTime.now(), Duration.ofMinutes(60));
         Subtask subtask12 = new Subtask(inMemoryTaskManager.nextId(), "Подзадача 12", "Описание",
-                TaskStatus.NEW, epic1.getID());
+                TaskStatus.NEW, epic1.getID(),
+                LocalDateTime.now(), Duration.ofMinutes(60));
         Subtask subtask13 = new Subtask(inMemoryTaskManager.nextId(), "Подзадача 13", "Описание",
-                TaskStatus.NEW, epic1.getID());
+                TaskStatus.NEW, epic1.getID(),
+                LocalDateTime.now(), Duration.ofMinutes(60));
         inMemoryTaskManager.addSubtask(subtask11);
         inMemoryTaskManager.addSubtask(subtask12);
         inMemoryTaskManager.addSubtask(subtask13);
