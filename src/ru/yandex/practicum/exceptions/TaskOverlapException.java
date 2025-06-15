a0d843cb5ee5fc52c7802b61a06c144d9e00c00b
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-06-15 20:24:47 +0300

    feat(TaskOverlapException): добавить новое исключение

---
+++ src/ru/yandex/practicum/exceptions/TaskOverlapException.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.exceptions;
+
+// Возникает, когда в TaskManager добавляется задача, имеющая
+// пересечение по времени выполнения с другой задачей, уже присутствующей в трекере
+public class TaskOverlapException extends RuntimeException {
+    // Конструктор класса TaskOverlapException
+    public TaskOverlapException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
