b53c35cd7e1647cb1258e78632eeecaebc3597f4
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-06-15 20:25:04 +0300

    refactor(): добавить комментарии в классы исключений

--- src/ru/yandex/practicum/exceptions/ManagerSaveException.java
+++ src/ru/yandex/practicum/exceptions/ManagerSaveException.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.exceptions;
 
+// Сигнализирует о проблеме при сохранении задач в файл в FileBackedTaskManager
 public class ManagerSaveException extends RuntimeException {
     // Конструктор класса ManagerSaveException
     public ManagerSaveException(String message, Throwable cause) {
