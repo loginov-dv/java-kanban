f87bdfdd7b22f2a3a490ca2f6b2d303da9c4890b
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-06-15 18:36:03 +0300

    feat(Managers): добавить методы создания конкретных реализаций трекеров задач

--- src/ru/yandex/practicum/taskManagement/Managers.java
+++ src/ru/yandex/practicum/taskManagement/Managers.java
@@ -1,11 +1,27 @@
 package ru.yandex.practicum.taskManagement;
 
+import java.io.File;
+import java.io.IOException;
+
 // Утилитарный класс для создания менеджеров задач
-public class Managers {
+public final class Managers {
 
     // Создать объект класса, реализующего TaskManager, по умолчанию
     public static TaskManager getDefault() {
+        return getInMemoryTaskManager();
+    }
+
+    // Создать объект класса InMemoryTaskManager
+    public static InMemoryTaskManager getInMemoryTaskManager() {
         return new InMemoryTaskManager(getDefaultHistory());
+    }
+
+    // Создать объект класса FileBackedTaskManager
+    public static FileBackedTaskManager getFileBackedTaskManager() throws IOException {
+        File saveFile = File.createTempFile("autoSave", ".csv");
+        saveFile.deleteOnExit();
+
+        return new FileBackedTaskManager(saveFile);
     }
 
     // Создать объект класса, реализующего HistoryManager, по умолчанию
