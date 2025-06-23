e4884373107337571613442e37b56cca5f7d6dbe
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-06-23 20:57:02 +0300

    feat(TasksHandler): добавить класс-обработчик пути /tasks и реализовать обработку GET /tasks

---
+++ src/ru/yandex/practicum/server/TasksHandler.java
@@ -0,0 +1,74 @@
+package ru.yandex.practicum.server;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Optional;
+
+import ru.yandex.practicum.tasks.Task;
+
+public class TasksHandler implements HttpHandler {
+    private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+
+    private final Gson gson = new GsonBuilder().create();
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        String path = exchange.getRequestURI().getPath();
+        String[] pathParts = path.split("/");
+
+        switch (method) {
+            case "GET":
+                if (pathParts.length == 2) {
+                    // GET /tasks
+                    List<Task> tasks = HttpTaskServer.manager.getAllBasicTasks();
+                    String tasksJson = gson.toJson(tasks);
+                    writeResponse(exchange, tasksJson, 200);
+                } else if (pathParts.length == 3) {
+                    // GET /tasks/{id}
+                } else {
+                    // throw
+                }
+            case "POST":
+                if (pathParts.length == 2) {
+                    // POST /tasks
+                } else {
+                    // throw
+                }
+            case "DELETE":
+                if (pathParts.length == 3) {
+                    // DELETE /tasks/{id}
+                } else {
+                    // throw
+                }
+            default:
+                writeResponse(exchange, "Такого эндпоинта не существует", 404);
+        }
+    }
+
+    private Optional<Integer> getIdFromPath(String path) {
+        String[] pathParts = path.split("/");
+        try {
+            return Optional.of(Integer.parseInt(pathParts[2]));
+        } catch (NumberFormatException exception) {
+            return Optional.empty();
+        }
+    }
+
+    private void writeResponse(HttpExchange exchange, String responseString,
+                               int responseCode) throws IOException {
+        try (OutputStream os = exchange.getResponseBody()) {
+            exchange.sendResponseHeaders(responseCode, 0);
+            os.write(responseString.getBytes(DEFAULT_CHARSET));
+        }
+        exchange.close();
+    }
+}
