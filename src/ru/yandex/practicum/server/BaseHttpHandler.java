fa56792169de9134047d882669c904f67735ff98
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-06-24 20:30:22 +0300

    refactor(BaseHttpHandler): добавить базовый класс BaseHttpHandler и перенести в него общие для всех обработчиков методы

---
+++ src/ru/yandex/practicum/server/BaseHttpHandler.java
@@ -0,0 +1,66 @@
+package ru.yandex.practicum.server;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+public class BaseHttpHandler {
+    private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+
+    protected Optional<Integer> getIdFromPath(String path) {
+        String[] pathParts = path.split("/");
+        try {
+            return Optional.of(Integer.parseInt(pathParts[2]));
+        } catch (NumberFormatException exception) {
+            return Optional.empty();
+        }
+    }
+
+    protected void writeResponse(HttpExchange exchange, String responseString,
+                               int responseCode) throws IOException {
+        try (OutputStream os = exchange.getResponseBody()) {
+            exchange.sendResponseHeaders(responseCode, 0);
+            os.write(responseString.getBytes(DEFAULT_CHARSET));
+        }
+        exchange.close();
+    }
+}
+
+// TypeAdapter для преобразования Duration в long
+class DurationAdapter extends TypeAdapter<Duration> {
+    @Override
+    public void write(final JsonWriter jsonWriter, final Duration duration) throws IOException {
+        jsonWriter.value(duration.toMinutes());
+    }
+
+    @Override
+    public Duration read(final JsonReader jsonReader) throws IOException {
+        return Duration.ofMinutes(jsonReader.nextLong());
+    }
+}
+
+// TypeAdapter для преобразования LocalDateTime в String
+class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+
+    @Override
+    public void write(final JsonWriter jsonWriter,
+                      final LocalDateTime localDateTime) throws IOException {
+        jsonWriter.value(localDateTime.format(dtf));
+    }
+
+    @Override
+    public LocalDateTime read(final JsonReader jsonReader) throws IOException {
+        return LocalDateTime.parse(jsonReader.nextString(), dtf);
+    }
+}
--- src/ru/yandex/practicum/server/TasksHandler.java
+++ src/ru/yandex/practicum/server/TasksHandler.java
@@ -1,18 +1,13 @@
 package ru.yandex.practicum.server;
 
 import com.google.gson.*;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
-import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -20,10 +15,7 @@ import java.util.Optional;
 import ru.yandex.practicum.managers.TaskManager;
 import ru.yandex.practicum.tasks.Task;
 
-public class TasksHandler implements HttpHandler {
-
-    private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
-
+public class TasksHandler extends BaseHttpHandler implements HttpHandler {
     private final Gson gson = new GsonBuilder()
             .registerTypeAdapter(Duration.class, new DurationAdapter())
             .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
@@ -137,51 +129,5 @@ public class TasksHandler implements HttpHandler {
             default:
                 writeResponse(exchange, "Такого эндпоинта не существует", 404);
         }
-    }
-
-    private Optional<Integer> getIdFromPath(String path) {
-        String[] pathParts = path.split("/");
-        try {
-            return Optional.of(Integer.parseInt(pathParts[2]));
-        } catch (NumberFormatException exception) {
-            return Optional.empty();
-        }
-    }
-
-    private void writeResponse(HttpExchange exchange, String responseString,
-                               int responseCode) throws IOException {
-        try (OutputStream os = exchange.getResponseBody()) {
-            exchange.sendResponseHeaders(responseCode, 0);
-            os.write(responseString.getBytes(DEFAULT_CHARSET));
-        }
-        exchange.close();
-    }
-}
-// TypeAdapter для преобразования Duration в long
-class DurationAdapter extends TypeAdapter<Duration> {
-    @Override
-    public void write(final JsonWriter jsonWriter, final Duration duration) throws IOException {
-        jsonWriter.value(duration.toMinutes());
-    }
-
-    @Override
-    public Duration read(final JsonReader jsonReader) throws IOException {
-        return Duration.ofMinutes(jsonReader.nextLong());
-    }
-}
-
-// TypeAdapter для преобразования LocalDateTime в String
-class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
-    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-
-    @Override
-    public void write(final JsonWriter jsonWriter,
-                      final LocalDateTime localDateTime) throws IOException {
-        jsonWriter.value(localDateTime.format(dtf));
-    }
-
-    @Override
-    public LocalDateTime read(final JsonReader jsonReader) throws IOException {
-        return LocalDateTime.parse(jsonReader.nextString(), dtf);
     }
 }
