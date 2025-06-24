0beeb5277d8bf5eadc86b6b4e635d596f15d320e
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-06-24 21:00:25 +0300

    feat(HttpTaskServer): добавить методы start() и stop()

--- src/ru/yandex/practicum/server/HttpTaskServer.java
+++ src/ru/yandex/practicum/server/HttpTaskServer.java
@@ -1,21 +1,57 @@
 package ru.yandex.practicum.server;
 
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import ru.yandex.practicum.managers.Managers;
 import ru.yandex.practicum.managers.TaskManager;
+import ru.yandex.practicum.tasks.Task;
+import ru.yandex.practicum.tasks.TaskStatus;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class HttpTaskServer {
+    // Порт
     private static final int PORT = 8080;
-    private static final TaskManager manager = Managers.getInMemoryTaskManager();
+    // Экземпляр класса, реализующего TaskManager
+    private final TaskManager taskManager;
+    // HttpServer
+    private HttpServer httpServer;
 
+    // Конструктор класса HttpTaskServer
+    public HttpTaskServer(TaskManager taskManager) {
+        this.taskManager = taskManager;
+    }
+
+    // Точка входа
     public static void main(String[] args) throws IOException {
-        HttpServer httpServer = HttpServer.create();
-        httpServer.bind(new InetSocketAddress(PORT), 0);
-        httpServer.createContext("/tasks", new TasksHandler(manager));
+        HttpTaskServer server = new HttpTaskServer(Managers.getInMemoryTaskManager());
+        server.start();
+    }
+
+    // Запустить сервер
+    public void start() throws IOException {
+        // test data
+        Task task1 = new Task(1, "Task1", "description", TaskStatus.NEW,
+                LocalDateTime.of(2025, 1, 1, 10, 0), Duration.ofMinutes(60));
+        Task task2 = new Task(2, "Task2", "description", TaskStatus.IN_PROGRESS,
+                LocalDateTime.of(2025, 1, 10, 10, 0), Duration.ofMinutes(60));
+        taskManager.addBasicTask(task1);
+        taskManager.addBasicTask(task2);
+        //
+
+        httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
+        httpServer.createContext("/tasks", new TasksHandler(taskManager));
         httpServer.start();
+
+        System.out.println("HTTP-сервер запущен на " + PORT + " порту");
+    }
+
+    // Остановить сервер
+    public void stop() {
+        if (httpServer != null) {
+            httpServer.stop(0);
+        }
     }
 }
