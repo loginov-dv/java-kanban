72d962fb3b39fac6b47b9ae64e21d45a8684b4bb
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-06-26 21:04:16 +0300

    fix(HttpTaskServer): убрать тестовые данные

--- src/ru/yandex/practicum/server/HttpTaskServer.java
+++ src/ru/yandex/practicum/server/HttpTaskServer.java
@@ -35,32 +35,6 @@ public class HttpTaskServer {
 
     // Запустить сервер
     public void start() throws IOException {
-        // test data
-        Task task1 = new Task(1, "Task1", "description", TaskStatus.NEW,
-                LocalDateTime.of(2025, 1, 1, 10, 0), Duration.ofMinutes(60));
-        Task task2 = new Task(2, "Task2", "description", TaskStatus.IN_PROGRESS,
-                LocalDateTime.of(2025, 1, 10, 10, 0), Duration.ofMinutes(60));
-        Task task3 = new Task(3, "Task3", "description", TaskStatus.IN_PROGRESS,
-                LocalDateTime.of(2025, 2, 10, 10, 0), Duration.ofMinutes(60));
-        taskManager.addBasicTask(task1);
-        taskManager.addBasicTask(task2);
-        taskManager.addBasicTask(task3);
-
-        Epic epic1 = new Epic(10, "Эпик 1", "Описание", TaskStatus.NEW);
-        Epic epic2 = new Epic(20, "Эпик 2", "Описание", TaskStatus.NEW);
-        taskManager.addEpic(epic1);
-        taskManager.addEpic(epic2);
-
-        Subtask subtask11 = new Subtask(11, "Подзадача 11", "Описание",
-                TaskStatus.IN_PROGRESS, epic1.getID(),
-                LocalDateTime.of(2028, 1, 1, 10, 0), Duration.ofMinutes(60));
-        Subtask subtask21 = new Subtask(21, "Подзадача 21", "Описание",
-                TaskStatus.DONE, epic2.getID(),
-                LocalDateTime.of(2028, 1, 10, 10, 0), Duration.ofMinutes(60));
-        taskManager.addSubtask(subtask11);
-        taskManager.addSubtask(subtask21);
-        //
-
         httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
         httpServer.createContext("/tasks", new TasksHandler(taskManager));
         httpServer.createContext("/subtasks", new SubtasksHandler(taskManager));
@@ -76,6 +50,7 @@ public class HttpTaskServer {
     public void stop() {
         if (httpServer != null) {
             httpServer.stop(0);
+            System.out.println("HTTP-сервер завершил работу");
         }
     }
 }
